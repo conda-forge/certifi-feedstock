6ce352dd72e677bfca43dbc280b37183dfa8fddf
Isuru Fernando <isuruf@gmail.com>
2020-09-16 20:33:42 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,3 @@
 # use bootstrapped pip to install certifi without depending on installed pip
-export PYTHONPATH=$PWD/pip_wheel:$PWD/setuptools_wheel:$PYTHONPATH
+export PYTHONPATH=$PWD/pip_wheel:$PWD/setuptools_wheel
 $PYTHON -m pip install ./certifi/ --ignore-installed --no-deps -vv
