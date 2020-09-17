f7718af48fe3e1f66f4c587523bcbbe9128e0415
Isuru Fernando <isuruf@gmail.com>
2020-09-16 19:32:33 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,3 @@
-if [[ ${target_platform} == "osx-arm64" ]]; then
-   sed -i.bak "s@release =@release = 10.16@g" $BUILD_PREFIX/venv/crossenv.cfg
-fi
-
 # use bootstrapped pip to install certifi without depending on installed pip
 export PYTHONPATH=$PWD/pip_wheel:$PWD/setuptools_wheel:$PYTHONPATH
 $PYTHON -m pip install ./certifi/ --ignore-installed --no-deps -vv
