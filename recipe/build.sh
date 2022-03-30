249575620069d32e92eb8958b648d5fe71693c10
jakirkham <jakirkham@gmail.com>
2022-03-29 22:55:24 -0700

    List libraries in `$BUILD_PREFIX`

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,4 @@
 # use bootstrapped pip to install certifi without depending on installed pip
+ls -1 "${BUILD_PREFIX}/lib"
 export PYTHONPATH=$PWD/pip_wheel:$PWD/setuptools_wheel
 $PYTHON -m pip install ./certifi/ --ignore-installed --no-deps -vv
